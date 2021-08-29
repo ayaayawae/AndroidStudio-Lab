544d90e49ade76bfef6e091367e89973942b87c0
Bonaventura Sanjaya <bonaventura.sanjaya@student.umn.ac.id>
2021-08-29 19:00:26 +0700

    week 2

--- Week02_BonaventuraSanjaya_00000038083/app/src/main/java/id/ac/umn/week02_bonaventurasanjaya_00000038083/MainActivity.java
+++ Week02_BonaventuraSanjaya_00000038083/app/src/main/java/id/ac/umn/week02_bonaventurasanjaya_00000038083/MainActivity.java
@@ -3,12 +3,65 @@ package id.ac.umn.week02_bonaventurasanjaya_00000038083;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText angka1, angka2;
+    TextView hasil;
+    Button btnTambah, btnKurang, btnKali, btnBagi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        angka1 = this.findViewById(R.id.angka1);
+        angka2 = this.findViewById(R.id.angka2);
+        hasil = this.findViewById(R.id.hasil);
+        btnTambah = this.findViewById(R.id.btnTambah);
+        btnKurang = this.findViewById(R.id.btnKurang);
+        btnKali = this.findViewById(R.id.btnKali);
+        btnBagi = this.findViewById(R.id.btnBagi);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { hitung('+'); }
+        });
+        btnKurang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { hitung('-'); }
+        });
+        btnKali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { hitung('*'); }
+        });
+        btnBagi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { hitung('/'); }
+        });
+    }
+
+    protected void hitung(char operator) {
+        try {
+            double operand1 = Double.parseDouble(angka1.getText().toString());
+            double operand2 = Double.parseDouble(angka2.getText().toString());
+            double result = 0.0;
+
+            switch (operator){
+                case('+') : result = operand1 + operand2; break;
+                case('-') : result = operand1 - operand2; break;
+                case('*') : result = operand1 * operand2; break;
+                case('/') : result = operand1 / operand2; break;
+            }
+
+            hasil.setText(String.valueOf(result));
+        } catch (Exception e) {
+            hasil.setText("Kedua Angka Harus Ditentukan");
+        }
+
+
+
     }
 }
